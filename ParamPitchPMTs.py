2b64116de194914e8d10de80fc503a8b6832e315
Gonzalo <gonzaponte@gmail.com>
2016-10-07 14:58:19 +0200

    sampling error param

---
+++ ParamPitchPMTs.py
@@ -0,0 +1,27 @@
+from ROOT import *
+
+
+F = TFile( "/Users/Gonzalo/github/elparametrization/PyScripts/ParamBuilder/NEW_S2PMTparametrization_plots.root")
+
+M = TMultiGraph()
+for ID in range(12):
+    f = F.Get("fit_" + str(ID))
+
+    G = TGraph()
+
+    pitch   = 1e-1
+    npoints = int(f.GetXmax()/pitch) - 1
+    j=0
+    for i in range(npoints):
+        x0 = i * pitch
+        x1 = x0 + pitch * 0.99999
+        try:
+            G.SetPoint(i-j,x0,abs(1-f.Eval(x1)/f.Eval(x0))+1e-8)
+        except:
+            j += 1
+    M.Add(G)
+C = TCanvas()
+M.SetTitle("Sampling error with a {0} mm pitch;Distance to center (mm);Relative sampling error".format(pitch))
+M.Draw('ap')
+C.Modified()
+C.SaveAs("pmt_sampling_error_{0}.png".format(pitch))
