11e78db27282cfbf0a021349408ac4618336bf7b
Gonzalo <gonzaponte@gmail.com>
2017-04-07 11:38:22 +0200

    print folders recursively

---
+++ directory_tree_printer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import os
+import sys
+import argparse
+
+def print_folder(folder="./", prefix="", skip=(), ignore_hidden=True, output_stream=sys.stdout):
+    for name in sorted(os.listdir(folder)):
+        if name in skip or (ignore_hidden and name[0] == "."):
+            continue
+        fullname = folder + name
+        if os.path.isfile(fullname):
+            print(prefix + name, file=output_stream)
+        elif os.path.isdir(fullname):
+            print(prefix + name + " --->", file=output_stream)
+            print_folder(fullname + "/", prefix + "\t", skip, ignore_hidden, output_stream)
+        else:
+            print("Not recognized:", fullname, file=output_stream)
+
+parser = argparse.ArgumentParser(__file__)#"DirectoryTreePrinter")
+parser.add_argument("-f", metavar="folder", type=str, help="root file where tree starts", required=True)
+parser.add_argument("-s", metavar="folder", type=str, nargs="+", help="file or folder names to be skipped")
+parser.add_argument("-l", metavar="logfile", type=str, help="log file")
+parser.add_argument("--show-hidden", action="store_true", help="show hidden files")
+
+nargs="+",
+flags, extras = parser.parse_known_args(sys.argv)
+if flags.s is None:
+    flags.s = ()
+print("Skipping the following folders:", *flags.s)
+print_folder(flags.f, skip=flags.s, ignore_hidden=not flags.show_hidden, output_stream=sys.stdout if flags.l is None else open(flags.l, "w"))
