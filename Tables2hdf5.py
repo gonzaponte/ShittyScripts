e1d745bcb1b37b6b32f2c6da966db42537595c35
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2020-06-30 02:21:51 +0300

    I don't know wtf is this

--- Tables2hdf5.py
+++ Tables2hdf5.py
@@ -1,76 +1,107 @@
-import tables as tb
-import numpy as np
+from __future__ import print_function
+
 import time
-from NEW_maps_201607 import EL_map,PMT_map,SiPM_map
+import numpy  as np
+import tables as tb
 
-ID2index = { ID : i for i,ID in enumerate(sorted(SiPM_map)) }
+from NEW_maps_201607 import   EL_map
+from NEW_maps_201607 import  PMT_map
+from NEW_maps_201607 import SiPM_map
 
-def FindData(f,ID):
+
+class ProbDescription(tb.IsDescription):
+    grid_xy   = tb.Float32Col(shape=2, pos=1) # Grid xy
+    sens_id   = tb.Int32Col  (shape=1, pos=2) # SensorIDs
+    sens_prob = tb.Float32Col(shape=1, pos=3) # Probabilities
+
+
+def FindData(f, ID):
     '''
         Find the lines corresponding to a certain ELpointID.
     '''
-    return filter( lambda x: x[1] == ID, f )
+    return filter(lambda x: x[1] == ID, f)
+
 
 def GetCathodeData(cdata):
     '''
         Pick the relevant information.
     '''
-    return np.array([ float(p[3])*20 for p in sorted(cdata,key=lambda x: int(x[2]))])
+    return { int(p[2]) : float(p[3])*20 for p in cdata }
+
 
 def GetAnodeData(adata):
     '''
         Pick the relevant information.
     '''
-    x = np.zeros(len(SiPM_map))
-    for p in sorted(adata,key=lambda x: int(x[2])):
-        x[ID2index[int(p[2])]] = sum(map(float,p[3:]))
-    return x
+    return {int(p[2]) : sum(map(float, p[3:])) for p in adata}
 
-def GetProbabilities( cdata, adata ):
+
+def FillProbabilities(cdata, adata, table, xy0):
     '''
         Return the array corresponding to some row.
     '''
-    return np.concatenate((GetCathodeData(cdata),GetAnodeData(adata)))
+    row  = table.row
+    cath = GetCathodeData(cdata)
+    anod = GetAnodeData  (adata)
+    for probs, sensormap in zip([cath, anod], [PMT_map, SiPM_map]):
+        for ID, prob in probs.items():
+            row['grid_xy'  ] = np.array(xy0)
+            row['sens_id'  ] = ID
+            row['sens_prob'] = prob
+            row.append()
+    table.flush()
 
-def BuildTable(CathodeFileName,AnodeFileName,outFileName):
+
+def FillSensorTable(table):
+    for sensormap in [PMT_map, SiPM_map]:
+        for ID, (x, y) in sorted(sensormap.items()):
+            sns = np.array([ID, x, y])
+            table.append(sns.reshape(1, 3))
+    table.flush()
+
+
+def BuildTable(CathodeFileName, AnodeFileName, outFileName):
     '''
         Build the table and store it in a hdf5 file.
     '''
-    fcathode = map( str.split, open(CathodeFileName,'r').readlines()[1:] )
-    fanode   = map( str.split, open(AnodeFileName,'r').readlines()[1:] )
+    fcathode = map(str.split, open(CathodeFileName, 'r').readlines()[1:])
+    fanode   = map(str.split, open(AnodeFileName  , 'r').readlines()[1:])
     
-    nsensors = len(PMT_map) + len(SiPM_map)
-    tablestr = []
-    with tb.open_file(outFileName, "w",filters=tb.Filters(complib="zlib", complevel=1)) as fileOut:
-        group = fileOut.create_group(fileOut.root,"GridData")
-#        xytable = fileOut.create_table(group, "XY", XYdescription,"ELpointID-XY map",tb.Filters(0))
-#        probstable = fileOut.create_table(group, "Probs",ProbDescription,"Probabilities",tb.Filters(0))
-        xytable = fileOut.create_earray(fileOut.root.GridData, "XY",
-                                        atom=tb.Float32Atom(),
-                                        shape=(0, 2),
-                                        expectedrows=len(EL_map))
-                                        
-        probstable = fileOut.create_earray(fileOut.root.GridData, "Probabilities",
-                                           atom=tb.Float32Atom(),
-                                           shape=(0, nsensors),
-                                           expectedrows=len(EL_map))
+    comp = tb.Filters(complib="zlib", complevel=1)
+    with tb.open_file(outFileName, "w",
+                      filters = comp) as fileOut:
+        sens_group = fileOut.create_group(fileOut.root, "Sensors")
+        prob_group = fileOut.create_group(fileOut.root, "Probabilities")
+        sens_table = fileOut.create_earray(fileOut.root.Sensors, "XY",
+                                           atom         = tb.Float32Atom(),
+                                           shape        = (0, 3),
+                                           expectedrows = len(PMT_map) + len(SiPM_map))
                                            
+        FillSensorTable(sens_table)
+        prob_table = fileOut.create_table(prob_group,
+                                          "data",
+                                          ProbDescription,
+                                          "probability map",
+                                          comp)
+        
         t0 = time.time()
-        for ELpointID,xy in sorted(EL_map.items()):
-            if not ELpointID % 10000: print ELpointID, time.time() - t0;t0 = time.time()
-            strID = str(ELpointID)
-            cathode_data  = FindData(fcathode,strID)
-            anode_data    = FindData(fanode,strID)
-            
-            xytable.append( np.array(xy).reshape((1,2)) )
-            probstable.append( GetProbabilities(cathode_data,anode_data).reshape((1,nsensors)) )
-    xytable.flush()
-    probstable.flush()
+        for ELpointID, xy0 in sorted(EL_map.items()):
+            if not ELpointID % 10000:
+                print(ELpointID, time.time() - t0)
+                t0 = time.time()
+
+            strID         = str(ELpointID)
+            cathode_data  = FindData(fcathode, strID)
+            anode_data    = FindData(fanode  , strID)
+            FillProbabilities(cathode_data,
+                              anode_data,
+                              prob_table,
+                              xy0)
     return
 
 if __name__ == '__main__':
     cFileName = '/Users/Gonzalo/Desktop/Tables/ReproducedCathode.txt'
     aFileName = '/Users/Gonzalo/Desktop/Tables/ReproducedAnode.txt'
-    oFileName = '/Users/Gonzalo/Desktop/TableS/ReproducedFull.h5'
-    BuildTable( cFileName, aFileName, oFileName )
+    oFileName = '/Users/Gonzalo/Desktop/Tablesta/ReproducedFull.h5'
+    BuildTable(cFileName, aFileName, oFileName)
 
