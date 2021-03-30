790e81893ee6c4747cc575758d277d23209a218b
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2021-03-30 14:22:25 +0300

    Accept folders in mergeh5

--- mergeh5.py
+++ mergeh5.py
@@ -1,6 +1,7 @@
 #!/usr/env/python
 
 import sys
+import glob
 import argparse
 
 from functools import reduce
@@ -16,16 +17,32 @@ def is_indexed_table(node):
     return isinstance(node, tb.Table) and node.indexed
 
 
+def parse_path(path):
+    if not os.path.isdir(path): return (path,)
+
+    filenames = sorted(glob.glob(os.path.join(path, "*")))
+    return tuple(map(parse_path, filenames))
+
+
+def flatten(items):
+    flattened = []
+    for item in items:
+        if isinstance(item, (tuple, list)):
+            flattened.extend(flatten(item))
+        else:
+            flattened.append(item)
+    return flattened
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_files", nargs="*")
+    parser.add_argument("input_files_or_path", type=parse_path, nargs="*")
     parser.add_argument("-o", "--output-file", default="merged.h5")
     parser.add_argument("--overwrite", action="store_true")
     
     args = parser.parse_args(sys.argv[1:])
-    
-    first, *rest = args.input_files
+
+    first, *rest = flatten(args.input_files_or_path)
 
     tb.copy_file(first, args.output_file, overwrite=args.overwrite)
 
